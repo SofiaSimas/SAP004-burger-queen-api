d5a580f9314e7a6dcfdfff004925a9060556e99a
Sofia <sofiasimas1992@gmail.com>
2020-08-28 09:13:56 -0300

    index api

--- api/index.js
+++ api/index.js
@@ -1,1 +1,25 @@
-// scripts aqui
+import express from 'express'
+import bodyParser from 'body-parser'
+import OrderRoutes from './server/routes/OrderRoutes';
+import ProductsRoutes from './server/routes/ProductsRoutes';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.use('/api/orders', OrderRoutes);
+app.use('/api/products', ProductsRoutes);
+
+// quando recebe uma rota não listada
+app.get('*', (req, res) => res.status(200).send({
+   message: 'Boas-vindas à API do Burger Queen!',
+}));
+
+app.listen(port, () => {
+   console.log(`Server is running on PORT ${port}`);
+});
+
+module.exports = app
