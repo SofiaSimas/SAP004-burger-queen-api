79294e14f558f175c6d658ef60f31238a023af64
Sofia <sofiasimas1992@gmail.com>
2020-08-28 09:13:27 -0300

    config

---
+++ api/server/src/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "example",
+    "database": "bq_api_development",
+    "host": "db",
+    "dialect": "postgres",
+  },
+  "test": {
+    "username": "postgres",
+    "password": "example",
+    "database": "bq_api_test",
+    "host": "db",
+    "dialect": "postgres",
+  },
+  "production": {
+    "username": "postgres",
+    "password": "example",
+    "database": "bq_api_production",
+    "host": "db",
+    "dialect": "postgres",
+  }
+}
